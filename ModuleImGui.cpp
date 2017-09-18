a61c32249861e4b41129bc67105db987fcd5f254
dreedcon <ssaezcalero@gmail.com>
2017-09-18 20:34:19 +0200

    Testing, input number and add two numbers

--- ModuleImGui.cpp
+++ ModuleImGui.cpp
@@ -21,26 +21,23 @@ update_status ModuleImGui::PreUpdate(float dt)
 {
 	ImGui_ImplSdlGL2_NewFrame(App->window->window);
 
-	
-	
+
+
 		if (ImGui::Button("Test Window"))
 			show_test_window = !show_test_window;
 		if (show_test_window)
 		{
 			ImGui::ShowTestWindow(&show_test_window);
 		}
-		if (ImGui::Button("Show Another Windo"))
-			show_another_window = !show_another_window;
-		if (show_another_window)
+		if (ImGui::Button("show prove window"))
+			show_prove_window = !show_prove_window;
+		if (show_prove_window)
 		{
-			    ImGui::Begin("Another Window", &show_another_window);
-				static float f = 0.0f;
-				ImGui::Text("Hello, world!");
-				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-				ImGui::ColorEdit3("clear color", (float*)&clear_color);
-				if (ImGui::Button("Test Window")) show_test_window ^= 1;
-				if (ImGui::Button("Another Window")) show_another_window ^= 1;
-				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			    ImGui::Begin("Prove Window", &show_another_window);
+				ImGui::InputInt("input number", &i0);
+				ImGui::InputInt("input number", &i1);
+				i2 = i1 + i0;
+				ImGui::InputInt("input number", &i2);
 				ImGui::End();
 		}
 		
@@ -66,3 +63,5 @@ bool ModuleImGui::CleanUp()
 
 	return true;
 }
+
+
--- ModuleImGui.h
+++ ModuleImGui.h
@@ -13,9 +13,15 @@ public:
 	update_status PreUpdate(float dt);
 	update_status Update(float dt);
 	bool CleanUp();
+
 private:
 	bool show_test_window = true;
 	bool show_another_window = false;
+	bool show_prove_window = false;
+	int i0 = 0;
+	int i1 = 0;
+	int i2 = 0;
+	
 	ImVec4 clear_color = ImColor(114, 144, 154);
 };
 
