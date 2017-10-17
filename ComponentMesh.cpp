aa19816d92a464459eb8eeeb9de64015c5f3a450
Andreu <Andreu.rbj@gmail.com>
2017-10-17 11:36:45 +0200

    now we draw textures with components

--- ComponentMesh.cpp
+++ ComponentMesh.cpp
@@ -55,33 +55,6 @@ const uint ComponentMesh::GetElementBuffer()
 }
 void ComponentMesh::Update()
 {
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glEnableClientState(GL_ELEMENT_ARRAY_BUFFER);
-	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-	glEnableClientState(GL_NORMAL_ARRAY);
-
-
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexPointer(3, GL_FLOAT, sizeof(Vertex), NULL);
-	glNormalPointer(GL_FLOAT, sizeof(Vertex), (void*)offsetof(Vertex, normals));
-	glTexCoordPointer(2, GL_FLOAT, sizeof(Vertex), (void*)offsetof(Vertex, tex_coords));
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-
-
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, NULL);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-
-
-
-	glDisableClientState(GL_VERTEX_ARRAY);
-	glDisableClientState(GL_ELEMENT_ARRAY_BUFFER);
-	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-	glDisableClientState(GL_NORMAL_ARRAY);
-
+	
 
 }
