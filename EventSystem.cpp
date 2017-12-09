397bf2e00588c65a2f0a9e470d8a70997290f6ca
Andreu <Andreu.rbj@gmail.com>
2017-12-09 13:56:42 +0100

    First working code of EventSystem (first try of function pointers)

--- EventSystem.cpp
+++ EventSystem.cpp
@@ -10,3 +10,22 @@ EventSystem::EventSystem()
 EventSystem::~EventSystem()
 {
 }
+
+void EventSystem::AddEvent(Event new_event)
+{
+	event_list[new_event.name] = new_event;
+}
+
+void EventSystem::CallEvent(const char * name)
+{
+	std::map<std::string, Event> ::iterator it = event_list.find(name);
+
+	if (it != event_list.end())
+	{
+		it->second.event_function();
+	}
+}
+
+Event::Event(const char* name, void(*ptr_function)()) :name(name), event_function(ptr_function)
+{
+}
--- EventSystem.h
+++ EventSystem.h
@@ -1,8 +1,27 @@
-#pragma once
+#ifndef _EVENT_SYSTEM_H
+#define _EVENT_SYSTEM_H
+#include <string>
+#include <functional>
+#include <map>
+// Function Pointers http://www.learncpp.com/cpp-tutorial/78-function-pointers/
+
+struct Event
+{
+	Event(const char* name, void(*ptr_function)());
+	std::string name;
+	std::function<void()> event_function;
+
+};
+//TODO add events with argurments
 class EventSystem
 {
 public:
 	EventSystem();
 	~EventSystem();
+	void AddEvent(Event new_event);
+	void CallEvent(const char* name);
+private:
+	std::map<std::string, Event> event_list;
 };
 
+#endif //_EVENT_SYSTEM_H
