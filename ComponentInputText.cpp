c13aec536c20e0a8aa4e795afbc8aba5b11fcef5
Andreu <Andreu.rbj@gmail.com>
2017-12-15 22:01:00 +0100

    Add basic functions on inpunt text

--- ComponentInputText.cpp
+++ ComponentInputText.cpp
@@ -1,5 +1,5 @@
 #include "ComponentInputText.h"
-
+#include "ComponentText.h"
 
 
 
@@ -10,4 +10,55 @@ ComponentInputText::ComponentInputText(GameObject * my_go):ComponentInteractive(
 
 ComponentInputText::~ComponentInputText()
 {
+}
+
+void ComponentInputText::AddText(const char * str)
+{
+	AddText(str, cursor_pos);
+}
+
+void ComponentInputText::AddText(const char * str, uint position)
+{
+	my_text->text_str.insert(position, str);
+}
+
+
+bool ComponentInputText::EraseText(uint position)
+{
+	//check
+	if (my_text->text_str.length() == 0) 
+		return false;
+
+	my_text->text_str.erase(position, 1);
+
+	//check if still have text
+	if (my_text->text_str.length() > 0)
+		return true;
+}
+
+void ComponentInputText::MoveRight()
+{
+	if (cursor_pos < my_text->text_str.length())
+	{
+		cursor_pos++;
+	}
+}
+
+void ComponentInputText::MoveLeft()
+{
+	if (cursor_pos >0)
+	{
+		cursor_pos--;
+	}
+}
+
+void ComponentInputText::MoveToEnd()
+{
+	cursor_pos = my_text->text_str.length();
+}
+
+void ComponentInputText::MoveToStart()
+{
+	cursor_pos =0;
+
 }
--- ComponentInputText.h
+++ ComponentInputText.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Globals.h"
 #include "Component.h"
 #include "ComponentInteractive.h"
 class ComponentText;
@@ -7,8 +8,17 @@ class ComponentInputText:public ComponentInteractive
 public:
 	ComponentInputText(GameObject* my_go);
 	~ComponentInputText();
+	void AddText(const char* str);
+	void AddText(const char* str, uint position);
+	bool EraseText(uint position);
+	void MoveRight();
+	void MoveLeft();
+	void MoveToEnd();
+	void MoveToStart();
 
 private:
+	uint cursor_pos = 0;
+
 	ComponentText* my_text = nullptr;
 };
 
